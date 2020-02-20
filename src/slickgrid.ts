3482d106253cd86b0d713875ffd100d0af0053f4
DanielRNichols <danielrnichols63@gmail.com>
2020-02-20 06:14:39 -0600

    Added defineGrids to slickgrid.ts

--- src/slickgrid.ts
+++ src/slickgrid.ts
@@ -1,17 +1,38 @@
+import {autoinject} from 'aurelia-framework';
+import { Column, GridOption } from 'aurelia-slickgrid';
 
+@autoinject
 export class SlickGrid {
   public heading = 'Slick Grid';
-   public tabledata: any[] = 
-      [
-        {id: 1, name: 'Jack'}, 
-        {id: 2, name: 'Arnie'}, 
-        {id: 3, name: 'Gary'}
+  public gridOptions: GridOption;
+  public columnDefinitions: Column[];
+  public tabledata: any[];
+
+  constructor() {
+    this.defineGrids();
+  }
+    
+
+  async activate(): Promise<void> {
+    this.tabledata = 
+    [
+      {id: 1, name: 'Jack'}, 
+      {id: 2, name: 'Arnie'}, 
+      {id: 3, name: 'Gary'}
+    ];
+    console.log(this.tabledata);
+  }
+
+     /* Define grid Options and Columns */
+     defineGrids() {
+      this.columnDefinitions = [
+        { id: 'id', name: 'Id', field: 'id', sortable: true, minWidth: 25 },
+        { id: 'name', name: 'Name', field: 'name', sortable: true, minWidth: 100 }
       ];
-
-
-
-  // async activate(): Promise<void> {
-  //   this.golfers = await this.dataService.golfers();
-  //   console.log(this.golfers);
-  // }
+      this.gridOptions = {
+        enableAutoResize: false,
+        enableSorting: true
+      };
+    }
+  
 }
